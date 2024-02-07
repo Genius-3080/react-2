83b3c624ddb864a2cc601f03c331d89b27d254f5
Genius-3080 <abdulboriytolqinov00200@gmail.com>
2024-02-08 03:57:32 +0500

    update-mode

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,46 @@ import poster6 from './img/poster-6.png'
 import icon from './img/dark.png'
 import profilePic from './img/profile image.png'
 import { useState } from 'react';
+import sun from './img/light.png'
 
 function App() {
-
+const [mode, setMode ]= useState(true)
+console.log(mode);
   return (
     
     <div className="App">
- <header>
+ <header className={mode ? 'darker' : 'sunner'}>
       <div class="container">
         <div class="heading">
-          <img class="icon" src={icon} alt="" />
+          <img onClick={()=> setMode(!mode)} class="icon" src={mode ? icon : sun} alt="" />
           <div class="profile__box">
             <img src={profilePic} alt="" />
             <div class="text__header__box">
-              <h1 class="header__text">Victor Alvarado</h1>
-              <p class="header__subtitle">Software Engineer</p>
+              <h1 className={mode ? 'header__text' : 'header__text sunner'}>Victor Alvarado</h1>
+              <p className={mode ? 'header__subtitle' : 'header__subtitle sunner'}>Software Engineer</p>
+       
+           
+       
+         
               <ul class="icon__pack">
-                <a href="#"> <i class="bx bxl-github"></i> </a>
-                <a href="#"> <i class="bx bxl-twitter"></i></a>
-                <a href="#"><i class="bx bxl-linkedin-square"></i></a>
-                <a href="#"><i class="bx bxl-youtube"></i></a>
+                <a href="#"><i className={mode ? 'bx bxl-github' : 'bx bxl-github sunner'}></i> </a>
+                <a href="#"><i className={mode ? 'bx bxl-twitter' : 'bx bxl-twitter sunner'}></i></a>
+                <a href="#"><i className={mode ? 'bx bxl-linkedin-square' : 'bx bxl-linkedin-square sunner'}></i></a>
+                <a href="#"><i className={mode ? 'bx bxl-youtube' : 'bx bxl-youtube sunner'}></i></a>
               </ul>
             </div>
           </div>
+       
           <div class="statics__box">
-            <div class="stat__1 stats">
+            <div className={mode ? 'stat__1 stats' : 'stat__1 stats sunner'}>
               7 <br />Years of work <br />
               experience
             </div>
-            <div class="stat__2 stats">
+            <div className={mode ? 'stat__2 stats' : 'stat__2 stats sunner'}>
               50+<br />Completed <br />
               projects
             </div>
-            <div class="stat__3 stats">
+            <div className={mode ? 'stat__3 stats' : 'stat__3 stats sunner'}>
               20+<br />Satisfied <br />
               customers
             </div>
@@ -50,11 +57,11 @@ function App() {
             <button class="btn__1">
               Download CV <i class="bx bxs-download"></i>
             </button>
-            <button class="btn__2">Contact me</button>
+            <button className={mode ? 'btn__2' : 'btn__2 shadower'}>Contact me</button>
           </div>
-          <div class="section__box">
-            <button class="secbtn__1">Portfolio</button>
-            <button class="secbtn__2">Skills</button>
+          <div className={mode ? 'section__box' : 'section__box shadower'}>
+            <button className={mode ? 'secbtn__1' : 'secbtn__1 grayer'}>Portfolio</button>
+            <button className={mode ? 'secbtn__2' : 'secbtn__2 shadower'}>Skills</button>
           </div>
           <div class="image__box">
            <img src={poster1} alt="" />
@@ -64,19 +71,12 @@ function App() {
            <img src={poster5} alt="" />
            <img src={poster6} alt="" />
           </div>
-          <h2 class="footer__text">
+          <h2 className={mode ? 'footer__text' : 'footer__text sunner'}>
             © Victor Alvarado. 2022 All rigths reserved
           </h2>
         </div>
       </div>
     </header>
-
-      
-
-
-
-
-
     </div>);
 }
 
